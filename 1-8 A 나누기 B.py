7678fd9d453761069e7a9153874a33a5bdeda4af
gasoline_car <shjeon09@naver.com>
2022-05-19 16:00:15 +0900

    first commit

--- 1-8 A 나누기 B.py
+++ 1-8 A 나누기 B.py
@@ -1,4 +1,13 @@
-data = input().split()
+# 문제
+# 두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+# 출력
+# 첫째 줄에 A×B를 출력한다.
+
+data = input() .split()
 A = int(data[0])
 B= int(data[1])
 print(A/B)
